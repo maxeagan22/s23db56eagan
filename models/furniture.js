3acb254149ea1f4233b91509d56ceea17febc0ee
maxeagan22 <124809445+maxeagan22@users.noreply.github.com>
2023-11-28 19:01:19 -0600

    Step 5 Complete
    Added validator to price

--- models/furniture.js
+++ models/furniture.js
@@ -3,7 +3,17 @@ const mongoose = require('mongoose');
 const furnitureSchema = new mongoose.Schema({
     name: String,
     finish: String,
-    price: Number 
+    price: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: function(value) {
+                // Custom validator function to check if the price is between 15 and 2000
+                return value >= 15 && value <= 2000;
+            },
+            message: 'Price must be between 15 and 2000.',
+        },
+    },
 });
 
 module.exports = mongoose.model('Furniture', furnitureSchema);
